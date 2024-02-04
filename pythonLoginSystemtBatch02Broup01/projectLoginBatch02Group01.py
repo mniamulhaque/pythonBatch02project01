0ab44cbcd9d016a204ce2d901e93961a2c1d131a
Niamul Haque <111523549+mniamulhaque@users.noreply.github.com>
2024-02-04 15:23:08 +0600

    Login Check Project
    Login Check Project

---
+++ pythonLoginSystemtBatch02Broup01/projectLoginBatch02Group01.py
@@ -0,0 +1,9 @@
+def log(id,password):
+    if id=='id@gmail.com' and password=='12345':
+        print("Your Id And Password Accepted,You Want to Visit your site")
+    else:
+        print("Your Id And Password did Not match")
+log(input('Your ID :'),input('Your Passwor :'))
+#Batch - 02 Create, Group - 01
+#name of grou member
+#1.Jannatul ,1.Jakiya, 3.Elma, 4.Anonna, 5.Sifa, 6.Mainor, 7.Rafi
